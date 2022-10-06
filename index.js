24be6ef7cff91b645f34d3914e2f0f092eafd0bc
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2022-10-06 09:36:49 +0800

    Processing of command line arguments using process.argv

---
+++ index.js
@@ -0,0 +1,23 @@
+const args = require('process');
+let argList = [];
+let argCount = 0;
+
+for (let i = 2; i < process.argv.length; i++) {
+    argList.push(process.argv[i]);
+    argCount++;
+}
+
+let combinedArg = argList.join(` `);
+let vowelCount = 0; 
+let iterator = 0;
+
+while(iterator < combinedArg.length) {
+
+    if (combinedArg[iterator].toLowerCase() === `a` || combinedArg[iterator].toLowerCase() === `e` || combinedArg[iterator].toLowerCase() === `i` || combinedArg[iterator].toLowerCase() === `o` || combinedArg[iterator].toLowerCase() === `u`) {
+        vowelCount++;
+    } 
+    iterator++;
+}
+
+console.log(`No. of Arguments: `, argCount);
+console.log(`Vowel Count of the Combined String: `, vowelCount);
