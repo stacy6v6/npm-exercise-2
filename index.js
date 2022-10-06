3ad119bccbb01baf4ecc07d71b1238c75c6393a0
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2022-10-06 10:29:23 +0800

    fixed template string and string format

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ for (let i = 2; i < process.argv.length; i++) {
     argCount++;
 }
 
-let combinedArg = argList.join(` `);
+let combinedArg = argList.join(' ');
 let vowelCount = 0; 
 let iterator = 0;
 
 while(iterator < combinedArg.length) {
 
-    if (combinedArg[iterator].toLowerCase() === `a` || combinedArg[iterator].toLowerCase() === `e` || combinedArg[iterator].toLowerCase() === `i` || combinedArg[iterator].toLowerCase() === `o` || combinedArg[iterator].toLowerCase() === `u`) {
+    if (combinedArg[iterator].toLowerCase() === 'a' || combinedArg[iterator].toLowerCase() === 'e' || combinedArg[iterator].toLowerCase() === 'i' || combinedArg[iterator].toLowerCase() === 'o' || combinedArg[iterator].toLowerCase() === 'u') {
         vowelCount++;
     } 
     iterator++;
 }
 
-console.log(`No. of Arguments: `, argCount);
-console.log(`Vowel Count of the Combined String: `, vowelCount);
+console.log(`No. of Arguments: ${argCount}`);
+console.log(`Vowel Count of the Combined String: ${vowelCount}`);
