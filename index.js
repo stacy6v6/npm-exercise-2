8fae6ba3b8156d614b72bb16edde8a061f3af3a5
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2022-10-11 08:47:27 +0800

    styling format changed by using prettier

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ let argList = [];
 let argCount = 0;
 
 for (let i = 2; i < process.argv.length; i++) {
-    argList.push(process.argv[i]);
-    argCount++;
+  argList.push(process.argv[i]);
+  argCount++;
 }
 
 let combinedArg = argList.join(' ');
-let vowelCount = 0; 
+let vowelCount = 0;
 let iterator = 0;
 
-while(iterator < combinedArg.length) {
-
-    if (combinedArg[iterator].toLowerCase() === 'a' || combinedArg[iterator].toLowerCase() === 'e' || combinedArg[iterator].toLowerCase() === 'i' || combinedArg[iterator].toLowerCase() === 'o' || combinedArg[iterator].toLowerCase() === 'u') {
-        vowelCount++;
-    } 
-    iterator++;
+while (iterator < combinedArg.length) {
+  if (
+    combinedArg[iterator].toLowerCase() === 'a' ||
+    combinedArg[iterator].toLowerCase() === 'e' ||
+    combinedArg[iterator].toLowerCase() === 'i' ||
+    combinedArg[iterator].toLowerCase() === 'o' ||
+    combinedArg[iterator].toLowerCase() === 'u'
+  ) {
+    vowelCount++;
+  }
+  iterator++;
 }
 
 console.log(`No. of Arguments: ${argCount}`);
